6f83fd77ac8b300ed996e55e8388631b8a1fb2f4
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-12-03 07:09:16 +0200

    Update main.py

--- CFA_API/api/main.py
+++ CFA_API/api/main.py
@@ -1,3 +1,15 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+from langchain.schema import Document
+# from langchain.llms import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from PyPDF2 import PdfReader
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 app = FastAPI()
